0a2dcfdceb189ab5b3558698241a5c72a059f97e
Ignas Obulaitis <ignasobulaitis@gmail.com>
2017-11-07 01:48:13 +0200

    Modified trainer to pass third test

--- trainer.rb
+++ trainer.rb
@@ -1,20 +1,12 @@
+require './account.rb'
 # Trainer class used for the workout and meal program
-class Trainer
-  @id = 0
-  class << self
-    attr_accessor :id
-  end
+class Trainer < Account
+  @count = 0
+
   def initialize
-    self.class.id += 1
-    @account_details = { account_id: 20_000 }
+    @information = {}
+    super()
+    @information[:account_type] = 20_000
   end
 
-  def register(full_name, email, password)
-    @account_details[:account_id] = 20_000 + self.class.id
-  end
-
-  def registered?
-    return true unless @account_details.fetch(:account_id).equal?(20_000)
-    false
-  end
 end
