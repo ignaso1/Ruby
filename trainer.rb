7b6c5ee49a7f2f18c3279af0969ffbadb764dd5a
Ignas Obulaitis <ignasobulaitis@gmail.com>
2017-11-06 02:33:46 +0200

    Trainer passed first test

--- trainer.rb
+++ trainer.rb
@@ -1,4 +1,20 @@
 # Trainer class used for the workout and meal program
 class Trainer
-  
+  @id = 0
+  class << self
+    attr_accessor :id
+  end
+  def initialize
+    self.class.id += 1
+    @account_details = { account_id: 20_000 }
+  end
+
+  def register(full_name, email, password)
+    @account_details[:account_id] = 20_000 + self.class.id
+  end
+
+  def registered?
+    return true unless @account_details.fetch(:account_id).equal?(20_000)
+    false
+  end
 end
--- trainer_spec.rb
+++ trainer_spec.rb
@@ -3,14 +3,16 @@ SimpleCov.start
 require './trainer.rb'
 
 describe Trainer do
-    context 'when not registered' do
-        before do
-          @trainer = Trainer.new
-        end
-
-        it 'can register' do
-          expect {@trainer.register('Ignas Obulaitis', 'ignastreneris@gmail.com', 'password')}
-            .to change { @trainer.registered? }.from(false).to(true)
-        end
+  context 'when not registered' do
+    before do
+      @trainer = Trainer.new
     end
+
+    #TODO: SEPARATE USER TO CLIENT AND USER CLASSES, ADD INHERITANCE
+
+    it 'can register' do
+      expect { @trainer.register('Jonas Gains', 'treneris@jonas.lt', '1234') }
+        .to change { @trainer.registered? }.from(false).to(true)
+    end
+  end
 end
