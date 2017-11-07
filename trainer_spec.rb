eef16587e6350e10def6d0b57c5210ae0ef55a7d
Ignas Obulaitis <ignasobulaitis@gmail.com>
2017-11-07 02:48:12 +0200

    Added workout plan test

--- trainer_spec.rb
+++ trainer_spec.rb
@@ -21,17 +21,23 @@ describe Trainer do
       @trainer.register('Jonas Gains', 'treneris@jonas.lt', '1234')
       @trainer.login('treneris@jonas.lt', '1234')
     end
-    
+
+    it 'can enter trainer\'s license number' do
+      expect { @trainer.license_number = 555_666_902 }
+        .to change { @trainer.preferences('license_number')}
+        .from(0).to(555_666_902)
+    end
+
     it 'can choose to create meal plans ' do
       expect { @trainer.create_meal_plans }
         .to change { @trainer.preferences('meal_plans')}
         .from(false).to(true)
     end
 
-    it 'can enter trainer\'s license number' do
-      expect { @trainer.license_number = 555_666_902 }
-        .to change { @trainer.preferences('license_number')}
-        .from(0).to(555_666_902)
+    it 'can choose to create workout plans ' do
+      expect { @trainer.create_workout_plans }
+        .to change { @trainer.preferences('workout_plans')}
+        .from(false).to(true)
     end
   end
 end
