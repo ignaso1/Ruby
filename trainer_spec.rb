0b3cc3a68025a324735bd824ea87dcb17f63b474
Ignas Obulaitis <ignasobulaitis@gmail.com>
2017-11-07 13:55:30 +0200

    Separated set prices test into four tests

--- trainer_spec.rb
+++ trainer_spec.rb
@@ -24,46 +24,55 @@ describe Trainer do
 
     it 'can enter trainer\'s license number' do
       expect { @trainer.license_number = 555_666_902 }
-        .to change { @trainer.preferences('license_number')}
+        .to change { @trainer.preferences('license_number') }
         .from(0).to(555_666_902)
     end
 
     it 'can choose to create meal plans ' do
       expect { @trainer.create_meal_plans }
-        .to change { @trainer.preferences('meal_plans')}
+        .to change { @trainer.preferences('meal_plans') }
         .from(false).to(true)
     end
 
     it 'can choose to create workout plans ' do
       expect { @trainer.create_workout_plans }
-        .to change { @trainer.preferences('workout_plans')}
+        .to change { @trainer.preferences('workout_plans') }
         .from(false).to(true)
     end
 
     it 'can choose to provide personal trainer services ' do
       expect { @trainer.provide_personal_trainer_services }
-        .to change { @trainer.preferences('trainer_services')}
+        .to change { @trainer.preferences('trainer_services') }
         .from(false).to(true)
     end
 
     it 'can choose to provide personal dietician services ' do
       expect { @trainer.provide_personal_dietician_services }
-        .to change { @trainer.preferences('dietician_services')}
+        .to change { @trainer.preferences('dietician_services') }
         .from(false).to(true)
     end
 
-    it 'can set prices of services' do
+    it 'can set the price of meal plans' do
       expect { @trainer.set_prices('meal_plans', 200) }
-        .to change { @trainer.preferences('meal_plans_price')}
+        .to change { @trainer.preferences('meal_plans_price') }
         .from(0).to(200)
+    end
+
+    it 'can set the price of workout plans' do
       expect { @trainer.set_prices('workout_plans', 200) }
-        .to change { @trainer.preferences('workout_plans_price')}
+        .to change { @trainer.preferences('workout_plans_price') }
         .from(0).to(200)
+    end
+
+    it 'can set the price of personal trainer services' do
       expect { @trainer.set_prices('trainer_services', 500) }
-        .to change { @trainer.preferences('trainer_services_price')}
+        .to change { @trainer.preferences('trainer_services_price') }
         .from(0).to(500)
+    end
+
+    it 'can set the price of personal dietician services' do
       expect { @trainer.set_prices('dietician_services', 500) }
-        .to change { @trainer.preferences('dietician_services_price')}
+        .to change { @trainer.preferences('dietician_services_price') }
         .from(0).to(500)
     end
   end
