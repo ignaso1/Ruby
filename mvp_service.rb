15335780a9c401d8f9db506577b510362cb85ab6
Ignas Obulaitis <ignasobulaitis@gmail.com>
2017-11-07 15:10:51 +0200

    Passed create ungeristered user account test

--- mvp_service.rb
+++ mvp_service.rb
@@ -4,8 +4,16 @@ require './user.rb'
 # Meal and workout program's service class, responsible for
 # interacting with user interface and managing all classes
 class MvpService
+  attr_reader :user
+
   def initialize
     @user = nil
+  end
+
+  def create_account(type)
+    return false unless type.eql?('User')
+    @user = User.new
+    true
   end
 
   def register_user(full_name, email, password)
