20725a679669f5b5d358db05f93981e45edc1dcf
Ignas Obulaitis <ignasobulaitis@gmail.com>
2017-11-07 15:20:50 +0200

    Added create unknown type account test, service_spec_helper

--- mvp_service_spec.rb
+++ mvp_service_spec.rb
@@ -1,7 +1,4 @@
-require 'simplecov'
-SimpleCov.start
-
-require './mvp_service.rb'
+require './service_spec_helper.rb'
 
 describe MvpService do
   before do
@@ -14,11 +11,16 @@ describe MvpService do
       .from(nil).to(User)
   end
 
+  it 'cannot create an account of unknown type' do
+    expect { @service.create_account('Animal') }
+      .not_to change { @service.user }
+      .from(nil)
+  end
+
   it 'can register a new user' do
-  	usr_name = 'Ignas'
-  	usr_password = '2017Ruby*10h'
+    usr_name = 'Ignas'
+    usr_password = '2017Ruby*10h'
     usr_email = 'ignas@obulaitis.it'
-    acc_type = 'User'
     expect(@service.register_user(usr_name, usr_email, usr_password))
       .to match('Registered successfully')
   end
---
+++ service_spec_helper.rb
@@ -0,0 +1,6 @@
+require 'simplecov'
+SimpleCov.start
+require './mvp_service.rb'
+require './account_spec.rb'
+require './user_spec.rb'
+require './trainer_spec.rb'
